40e46b40aa75ee5cec9a9d3644e83ee20c78a576
mahesh kharad <mkharad20@gmail.com>
2024-06-07 18:37:08 +0530

    first commit

---
+++ src/EmployeeData.js
@@ -0,0 +1,20 @@
+ export const EmployeeData = [
+    {
+        id: 1,
+        firstName: 'Mark',
+        lastName: 'David',
+        age: 24
+    },
+    {
+        id: 2,
+        firstName: 'Virat',
+        lastName: 'Kholi',
+        age: 25
+    },
+    {
+        id: 3,
+        firstName: 'Rohit',
+        lastName: 'Sharma',
+        age: 30
+    }
+]
